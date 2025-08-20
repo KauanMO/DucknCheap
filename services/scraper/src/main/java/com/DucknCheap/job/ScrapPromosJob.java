ff8e15f793336be063de31e1a9c1091b00962e0d
Kauan <kauaanmatheus@gmail.com>
2025-08-20 13:07:27 -0300

    feat: scrapping and sending promo to telegram bot notifier

--- services/scraper/src/main/java/com/DucknCheap/job/ScrapPromosJob.java
+++ services/scraper/src/main/java/com/DucknCheap/job/ScrapPromosJob.java
@@ -1,11 +1,14 @@
 package com.DucknCheap.job;
 
+import com.DucknCheap.RabbitMQ.promo.PromoNotifierPublisher;
 import com.DucknCheap.dto.CreatePromoDTO;
 import com.DucknCheap.service.ProductScrapperService;
 import com.DucknCheap.service.product.ProductService;
 import com.DucknCheap.service.promo.PromoService;
 import com.duckncheap.model.Product;
 import com.duckncheap.model.Promo;
+import com.duckncheap.rabbitmq.ProductInfoMessage;
+import com.duckncheap.rabbitmq.PromoInfoMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -19,6 +22,7 @@ public class ScrapPromosJob {
     private final ProductService productService;
     private final PromoService promoService;
     private final ProductScrapperService scrapperService;
+    private final PromoNotifierPublisher promoNotifierPublisher;
 
     @Scheduled(cron = "0 0 0/3 * * ?")
     public void execute() {
@@ -39,11 +43,16 @@ public class ScrapPromosJob {
 
     private void handleNewPromo(Product product, Double price, List<Promo> activePromos) {
         boolean alreadyExists = activePromos.stream()
-                .anyMatch(p -> p.getPrice().equals(price));
+                .anyMatch(p -> p.getPrice().equals(price) && p.getActive());
 
         if (!alreadyExists) {
             deactivateActivePromos(activePromos);
             promoService.create(new CreatePromoDTO(product, price));
+
+            promoNotifierPublisher.sendPromoNotifierMessage(
+                    new PromoInfoMessage(product.getUser().getChatId(),
+                            price,
+                            new ProductInfoMessage(product)));
         }
     }
 
